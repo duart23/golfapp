58972840d337aa231564b8aa20a08b71d9c8a33d
duart23 <duarte.peixoto.jorge@gmail.com>
2025-02-24 12:42:42 +0100

    Revert "db"
    This reverts commit 4dceb05d372aa8370b5b42ef8069a57390714bdf.

--- prisma/migrations/20250221110943_user_update/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `birthday` on the `user` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `user` DROP COLUMN `birthday`,
-    ADD COLUMN `dateOfBirth` DATETIME(3) NULL;
