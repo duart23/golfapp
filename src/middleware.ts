1b8b2937770f4811ef4d18817cd15ad70005efa8
duart23 <duarte.peixoto.jorge@gmail.com>
2025-03-13 12:16:24 +0100

    middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,14 @@ export default withAuth({
   pages: {
     signIn: "/login",
   },
+  callbacks: {
+    authorized: ({ req, token }) => {
+      if (req.nextUrl.pathname === "/loggedin" && !token) {
+        return false; // Redirect to login if no session
+      }
+      return true; // Allow access if logged in
+    },
+  },
 });
 
 export const config = {
